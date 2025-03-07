7faceb199c8eec43d4b35736ff5eae621d4c6715
MalinGjerde <599034@stud.hvl.no>
2025-03-07 12:34:49 +0100

    test

--- tests/test_part_a.py
+++ tests/test_part_a.py
@@ -1,5 +1,7 @@
 from unittest import TestCase, main
-from domain import SmartHouse
+from pathlib import Path
+import sys
+sys.path.append(str(Path().parent.absolute()))
 from demo_house import DEMO_HOUSE as h
 
 class TestPartA(TestCase):
@@ -14,7 +16,7 @@ class TestPartA(TestCase):
 
     def test_basic_get_no_of_devices(self):
         self.assertEqual(len(h.get_devices()), 14)
-"""
+
     def test_basic_get_device_by_id(self):
         # device id does not exist
         self.assertIsNone(h.get_device_by_id("9e5b8274-4e77-4e8e-80d2-b40d648ea04b"))
@@ -93,7 +95,7 @@ class TestPartA(TestCase):
         self.assertEqual(dresser, bulp.room)
         self.assertEqual(len(dresser.devices), 1)
         self.assertEqual(len(gr2.devices), 0)
-    """
+    
 
 if __name__ == "__main__":
     main()
